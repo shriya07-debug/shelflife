89568cd54a74c69b0e5a49b62b27e1b88b44c0ea
anubhavsilwal <anubhavsilwal@gmail.com>
2026-07-05 04:29:43 +0545

    v5.0 cleanup: remove failed-v5 orphans + junk

--- lib/repo/profile_repo.dart
+++
@@ -1,8 +0,0 @@
-import '../model/user_profile.dart';
-
-abstract class ProfileRepo {
-  Future<UserProfile?> get(String uid);
-  Future<void> save(String uid, UserProfile profile);
-  Future<void> update(String uid, Map<String, dynamic> fields);
-  Future<void> delete(String uid);
-}
